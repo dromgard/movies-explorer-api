89112a5833387d61b4d06a0ea38a32e4ab453576
Sergei Korolev <dromgard@yandex.ru>
2022-12-22 21:30:12 +0300

    #3 Подготовить readme, готово к 1 КР

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# movies-explorer-api
+## Данные облачного сервера
+
+IP: 62.84.117.255
+
+Backend: https://api.diploma-serhio.nomoredomains.club
+
+## Запуск проекта
+
+`npm run start` — запускает сервер.
+`npm run dev` — запускает сервер с hot-reload.
--- app.js
+++ app.js
@@ -19,12 +19,12 @@ const NotFoundError = require('./errors/NotFoundError');
 // Получаем логгеры.
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 const app = express();
 
 mongoose.set({ runValidators: true });
-mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 
 // Обработка res.body в json.
 app.use(bodyParser.json());
